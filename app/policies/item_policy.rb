644c363035e0c1e289edddb922f1df61d3b71a45
guido caldara <guido.caldara@gmail.com>
2018-05-23 11:53:51 +0800

    Added an end missing in controller

--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -15,23 +15,21 @@ class ItemPolicy
   end
 
   def create?
-
   end
 
   def new?
-
   end
 
   def update?
-    @record.user_id == @user.id) || @user.admin
+    @record.user_id == @user.id || @user.admin
   end
 
   def edit?
-    @record.user_id == @user.id) || @user.admin
+    @record.user_id == @user.id || @user.admin
   end
 
   def destroy?
-    @record.user_id == @user.id) || @user.admin
+    @record.user_id == @user.id || @user.admin
   end
 
 end
