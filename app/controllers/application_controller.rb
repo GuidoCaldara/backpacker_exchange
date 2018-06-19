12bca4dbe4bbc5cbc8494a8d9d65ff03b37700a5
guido caldara <guido.caldara@gmail.com>
2018-06-19 18:13:18 +0800

    metatag ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  protect_from_forgery
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
 
 
   def after_sign_in_path_for(resource_or_scope)
